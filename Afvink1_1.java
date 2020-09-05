33e11b707c7b6ae2afa1e76e1cfcc984596b67a4
Herke22 <55189463+Herke22@users.noreply.github.com>
2020-09-05 10:41:12 +0200

    Add files via upload

---
+++ Afvink1_1.java
@@ -0,0 +1,6 @@
+public class Afvink1_1 {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
---
+++ afvink1_2.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class afvink1_2 {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World");
+    }
+}
